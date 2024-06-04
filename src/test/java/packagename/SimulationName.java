e8b721f9ba9aec6dc071f445f06f48c5f8eb5f16
katwaGatling <katwa@gatling.io>
2024-06-04 18:03:00 +0200

    update headers

--- src/test/java/packagename/SimulationName.java
+++ src/test/java/packagename/SimulationName.java
@@ -12,11 +12,8 @@ public class SimulationName extends Simulation {
 
   HttpProtocolBuilder httpProtocol =
       http.baseUrl("https://computer-database.gatling.io")
-          .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-          .acceptLanguageHeader("en-US,en;q=0.5")
-          .acceptEncodingHeader("gzip, deflate")
-          .userAgentHeader(
-              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/119.0");
+      .acceptHeader("application/json")
+      .contentTypeHeader("application/json");
 
   ScenarioBuilder users = scenario("Scenario 1").exec(http("Home").get("/computers"));
 
